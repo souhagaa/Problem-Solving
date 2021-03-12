98f7fbee908e2f9a2184133e5282a702cdabfe5f
souhagaa <souha.gaaloul@hotmail.com>
2021-03-13 00:29:05 +0100

    optimizing rot13

--- rot13.js
+++ rot13.js
@@ -1,16 +1,20 @@
+var codeA = 'A'.charCodeAt(0),
+    codeZ = 'Z'.charCodeAt(0),
+    codea = 'a'.charCodeAt(0),
+    codez = 'z'.charCodeAt(0);
 function rot13(message){
 
   var encrypted = message.split('').map(char => { 
       let ascii = char.charCodeAt()
-      if (ascii >= 97 && ascii <= 122) { 
-          if(ascii+13 > 122) {
-              return String.fromCharCode(97 + 12 - (122 - ascii))
+      if (ascii >= codea && ascii <= codez) { 
+          if(ascii+13 > codez) {
+              return String.fromCharCode(codea + 12 - (codez - ascii))
             }
           return String.fromCharCode(ascii+13)
       }
-      else if (ascii >= 65 && ascii <= 90) { 
-          if(ascii+13 > 90) {
-              return String.fromCharCode(65 + 12 - (90 - ascii))
+      else if (ascii >= codeA && ascii <= codeZ) { 
+          if(ascii+13 > codeZ) {
+              return String.fromCharCode(codeA + 12 - (codeZ - ascii))
             }
           return String.fromCharCode(ascii+13)
       }
